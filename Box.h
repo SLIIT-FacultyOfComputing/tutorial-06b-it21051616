9f596652ccbcbe27ce6175545e1f444dd381beb8
IT21051616 <88090452+it21051616@users.noreply.github.com>
2021-08-18 12:56:24 +0000

    tute 06

--- Box.h
+++ Box.h
@@ -5,7 +5,11 @@ class Box {
        int height;
     public:
        // write prototypes of setters for length, width and height
+       
        // write prototypes of getters for length, width and height
+       void setLength(int l);
+       
+
  
        int calcVolume();
 };
--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,18 @@
 #include "Student.h"
 #include <iostream>
+#include<cstring>
+using namespace std;
 
 // Assign studentId and name
-Student::assignDetails() {
+void Student::assignDetails(int sID,char sname[]) {
+  StudentID = sId;
+  strcpy(name, sname);
   
 }
 
 // Display StudentId and Name
-Student::display() {
+void Student::display() {
+  cout<< "Student ID"<<studentID <<endl;
+  cout<< "Student Name"<< name <<endl;
   
 }
